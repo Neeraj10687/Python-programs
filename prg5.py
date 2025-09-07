c4523683600e7bed401b92f62336ee67872b5421
Neeraj <109914099+Neeraj10687@users.noreply.github.com>
2025-09-07 18:15:54 +0530

    program 6 to prg 12

--- prg5.py
+++ prg5.py
@@ -1,9 +1,9 @@
-li_name=['neeraj','bennly','thomas']
+li_name=['alucard','bennly','thomas']
 count=0
 for x in li_name:
 
 
     for a in x:
-        if a =='n':
+        if a =='a':
              count=count+1
 print(count)
